f2191a0945a96008ec198a74eb3770c679fbd2f4
kimgaejin <kimgaejin@naver.com>
2020-06-30 01:59:52 +0900

    플레이어 점프
    플레이어가 1단 점프만 가능하도록 수정

---
+++ Assets/3.Script/PlayerGroundCollider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundCollider : MonoBehaviour
+{
+    private bool isGround;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "ground" || collision.tag == "object")
+        {
+            isGround = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "ground" || collision.tag == "object")
+        {
+            isGround = false;
+        }
+    }
+
+    public bool GetIsGround()
+    {
+        return isGround;
+    }
+}
--- Assets/3.Script/PlayerMovement.cs
+++ Assets/3.Script/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private GameObject playerObject;
     private Rigidbody2D rigidbody2d;
+    private PlayerGroundCollider playerGroundCollider;
 
     private float moveSpeed = 1.0f;
     private float jumpPower = 3.0f;
@@ -15,19 +16,35 @@ public class PlayerMovement : MonoBehaviour
     {
         playerObject = this.gameObject;
         rigidbody2d = playerObject.GetComponent<Rigidbody2D>();
+        playerGroundCollider = playerObject.transform.Find("GroundCollider").GetComponent<PlayerGroundCollider>();
+
     }
 
     void Update()
     {
-        // move
+        if (Input.anyKey)
+        {
+            Move();
+            Jump();
+        }
+    }
+
+    private void Move()
+    {
         Vector3 arrow = Vector3.zero;
         if (Input.GetKey(KeyCode.LeftArrow)) arrow += Vector3.left;
         if (Input.GetKey(KeyCode.RightArrow)) arrow += Vector3.right;
         arrow = arrow.normalized * moveSpeed * Time.deltaTime;
         playerObject.transform.Translate(arrow, Space.World);
+    }
 
-        // jump
-        if (Input.GetKeyDown(KeyCode.S)) 
-            rigidbody2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+    private void Jump()
+    {
+        if (!Input.GetKeyDown(KeyCode.S)) return;
+
+        bool isGround = rigidbody2d.velocity.y <= 0.01f && playerGroundCollider.GetIsGround() == true;
+        if (isGround == false) return;
+
+        rigidbody2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
     }
 }
