3740325860031548a314830e409cb11f614ad9e5
kimgaejin <kimgaejin@naver.com>
2020-06-30 12:45:28 +0900

    플레이어 연속공격
    플레이어 연속공격 구현함. 3단 공격까지 가능하며 공격 사이사이 검 모션이 2번 나오는 오류 확인 요망

---
+++ Assets/3.Script/PlayerAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    GameObject playerObject;
+    Animator playerAttackAnimator;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerObject = this.gameObject;
+        playerAttackAnimator = playerObject.transform.Find("SwordGraphics").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerAttackAnimator.SetBool("isAttack", true);
+        }
+        else
+        {
+            playerAttackAnimator.SetBool("isAttack", false);
+
+        }
+    }
+}
