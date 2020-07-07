420e7130907d20c214b0da3a3516c84809cabc6d
kimgaejin <kimgaejin@naver.com>
2020-07-07 23:35:53 +0900

    다수 파티클 매니징 추가
    1개이상의 파티클을 사용할 때 이미 사용중인 파티클이 있으면 다음 파티클을 사용하도록 수정. 더불어 Monobehavior에서 상속받는 Particle이 new로 선언하면 fake object라고하여 AddComponent로 수정함.

---
+++ Assets/3.Script/Particle.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    public bool use;
+    public Transform trans;
+    public Sprite sprite;
+
+    public void Init (Transform trans)
+    {
+        this.trans = trans;
+        use = false;
+        trans.gameObject.SetActive(false);
+    }
+
+    public void Effusion(Vector3 pos)
+    {
+        if (!trans) return;
+
+        trans.gameObject.SetActive(true);
+        use = true;
+        foreach (Transform tran in trans)
+        {
+            tran.position = pos;
+
+            Vector2 arrow = Vector2.zero;
+            arrow.x = 1.5f - (Random.value * 3);
+            arrow.y = 3 + (int)(Random.value * 3);
+            Rigidbody2D rigid = tran.GetComponent<Rigidbody2D>();
+            rigid.velocity = Vector2.zero;
+            rigid.AddForce(arrow, ForceMode2D.Impulse);
+        }
+
+        StartCoroutine(ExitParticle(3.0f));
+    }
+
+
+    public IEnumerator ExitParticle(float time)
+    {
+        yield return new WaitForSeconds(time);
+        trans.gameObject.SetActive(false);
+        use = false;
+        yield break;
+    }
+
+}
--- Assets/3.Script/ParticleManager.cs
+++ Assets/3.Script/ParticleManager.cs
@@ -2,31 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Particle
-{
-    public bool enable;
-    public Transform trans;
-    public Sprite sprite;
-
-    public Particle(Transform trans)
-    {
-        this.trans = trans;
-    }
-
-    public void Effusion(int size)
-    {
-        if (!trans) return;
-        foreach (Transform tran in trans)
-        {
-            Vector2 arrow = Vector2.zero;
-            arrow.x = 1.5f - (Random.value * 3);
-            arrow.y = 3 + (int)(Random.value * 3);
-            tran.GetComponent<Rigidbody2D>().AddForce(arrow, ForceMode2D.Impulse);
-        }
-    }
-}
-
-
 public class ParticleManager : MonoBehaviour
 {
     private List<Particle> particles;
@@ -36,7 +11,10 @@ public class ParticleManager : MonoBehaviour
         particles = new List<Particle>();
         foreach (Transform tran in this.transform)
         {
-            particles.Add(new Particle(tran));
+            Particle particle = tran.gameObject.AddComponent<Particle>();
+            particle.Init(tran);
+            particles.Add(particle);
+            
         }
     }
 
@@ -44,7 +22,14 @@ public class ParticleManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-            particles[0].Effusion(3);
+            foreach (Particle particle in particles)
+            {
+                if (particle.use == false)
+                {
+                    particle.Effusion(Vector3.zero);
+                    break;
+                }
+            }
         }
     }
 
